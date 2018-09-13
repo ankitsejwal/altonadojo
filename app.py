16ef1860b368b21693c6ca1a6e7d59deb188abdd
ankitsejwal <ankit@sejw.al>
2018-09-13 22:17:13 +1000

    updated layouts and templates

--- app.py
+++ app.py
@@ -6,28 +6,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome'
+    return render_template('index.html')
 
 
 @app.route('/term')
 def term():
-    return 'Term'
+    return render_template('term.html')
 
 
 @app.route('/parent')
 def parent():
-    pass
+    return render_template('parent.html')
 
 
 @app.route('/child')
 def child():
-    pass
+    return render_template('child.html')
+
+
+@app.route('/volunteer')
+def volunteer():
+    return render_template('volunteer.html')
 
 
 @app.route('/admission')
 def admission():
-    pass
+    return render_template('admission.html')
 
 
 debug = os.environ.get('FLASK_DEBUG')
-app.run(port=5001, debug=debug)
+app.run(port=5002, debug=debug)

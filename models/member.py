ad2725494d0bc46cf490db00de8ed11b911b7725
ankitsejwal <ankit@sejw.al>
2018-11-06 13:50:20 +0530

    add birth year to members table

--- models/member.py
+++ models/member.py
@@ -8,6 +8,7 @@ class Member(db.Model):
     last_name = db.Column(db.String, nullable=False)
     member_type = db.Column(db.String, nullable=False)
     gender = db.Column(db.String, nullable=True)
+    birth_year = db.Column(db.Integer, nullable=True)
     email = db.Column(db.String, nullable=True)
     phone = db.Column(db.Integer, nullable=True)
     school = db.Column(db.String, nullable=True)
